6926c267cf2f5bd1591afccad958b6922caeba74
kolaAdetola <kolawoleadetola630@gmail.com>
2024-06-30 22:10:52 +0100

    updated quotes

--- project.js
+++ project.js
@@ -4,32 +4,32 @@ let persons = document.querySelector('.person');
 
 const quotes = [
     {
-    quote:`it does not matter how slowly you go as long as you don't stop`,
-    person:`conficius`
+        quote:`it does not matter how slowly you go as long as you don't stop`,
+        person:`conficius`
     },
     {
-    quote:`the journey of a thousand miles begins with a single step`,
-    person:`lao Tzu`
+        quote:`the journey of a thousand miles begins with a single step`,
+        person:`lao Tzu`
     },
     {
-    quote:`sometimes not getting what you want may be a mighty stroke of luck`,
-    person:`Dalai lama`
+        quote:`sometimes not getting what you want may be a mighty stroke of luck`,
+        person:`Dalai lama`
     },
     {
-    quote:`a word is enough for the wise. but it is never enough for the foolish`,
-    person:`austin ikpe king`
+        quote:`a word is enough for the wise. but it is never enough for the foolish`,
+        person:`austin ikpe king`
     },
     {
-    quote:`Be yourself; everyone else is already taken.`,
-    person:`Oscar Wilde`
+        quote:`Be yourself; everyone else is already taken.`,
+        person:`Oscar Wilde`
     },
     {
-    quote:`I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.`,
-    person:`Marilyn Monroe`
+        quote:`I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.`,
+        person:`Marilyn Monroe`
     },
     {
-    quote:`Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.`,
-    person:`Albert Einstein`
+        quote:`Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.`,
+        person:`Albert Einstein`
     },
     {
         quote: "In our society, letting others find out that you're a nice person is a very risky move. It's extremely likely that someone would take advantage of that.",
@@ -63,11 +63,30 @@ const quotes = [
         quote: `It's not the face that makes someone a monster; it's the choices they make with their lives.`,
         person: "Naruto Uzumaki"
     },
-    
+    {
+        quote: `Many of life’s failures are people who did not realize how close they were to success when they gave up.`,
+        person:`Thomas A. Edison`,
+    },
+    {
+        quote: `You only live once, but if you do it right, once is enough.`,
+        person:`Mae West`,
+    },
+    {
+        quote: `Get busy living or get busy dying.`,
+        person:` Stephen King`,
+    },
+    {
+        quote: `Money and success don’t change people; they merely amplify what is already there.`,
+        person:`Will Smith`,
+    },
+    {
+        quote: `Not how long, but how well you have lived is the main thing.`,
+        person:`Seneca`,
+    }
 ]
 btn.addEventListener('click', () => {
     let random = Math.floor(Math.random() * quotes.length);
-
+    
     quote.textContent = quotes[random].quote;
     persons.textContent = quotes[random].person;
 })
